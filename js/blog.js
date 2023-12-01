0c3d29d2bfd47e5cb2784a5e8a05c6aa9b0e6a75
unknown <lethanhnhon113@gmail.com>
2023-12-01 12:44:11 +0700

    content from page list to blog page

--- js/blog.js
+++ js/blog.js
@@ -2,3 +2,44 @@
 // 1: get blogId from URL
 // 2: blogId => blog (localStorage blogs)
 // 3: blog => DOM loader => generate data html
+function getBlogs() {
+    const blogString = localStorage.getItem('blogs') ?? '[]';
+    return JSON.parse(blogString);
+}
+console.log(getBlogs())
+function getBlog() {
+    const blogs = getBlogs();
+    const urlParams = new URLSearchParams(window.location.search);
+    const blogID = urlParams.get('blogId');
+    return blogs.find((blog) => blog.id === blogID) || {};
+}
+console.log(getBlog())
+
+document.addEventListener("DOMContentLoaded", () => {
+    const main = document.getElementById('main');
+    const blog = getBlog();
+
+    if (blog.id){
+        const content =
+            `       
+                    <div class="group">
+                        <div class="title">
+                            <h2>
+                                ${blog.title};
+                            </h2>
+                        </div>
+                        <div class="image">
+                            ${blog.image};
+                        </div>
+                        <div class="content">
+                            ${blog.content};
+                        </div>
+    
+                    </div>   
+            `;
+        main.innerHTML = `${content}`
+
+    }else {
+        main.innerText='không có'
+    }
+    })
